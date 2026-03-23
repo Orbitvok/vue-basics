e4b5e065fd6b2ed93325ab046cf51e9f0f4a9d8a
Дима <orbitvok.t@gmail.com>
2026-03-23 18:35:40 +0800

    add

---
+++ vue1/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Привет, Дима!</h1>
+    <p>Это моё первое приложение на Vue.js</p>
+  </div>
+  <div>
+    <h1>Счётчик: {{ count }}</h1>
+    <button @click="increment">Увеличить</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+</script>
