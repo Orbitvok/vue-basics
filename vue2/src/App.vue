47d2b5a1887fb42bdaa7e4cc386a73b66c5711e3
Дима <orbitvok.t@gmail.com>
2026-03-23 18:42:00 +0800

    add

---
+++ vue2/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello {{ msg }}</h1>
+  <input v-model="msg" />
+  <p>Счётчик: {{ count }}</p>
+  <button @click="count++">Увеличить</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('World')
+const count = ref(0)
+</script>
