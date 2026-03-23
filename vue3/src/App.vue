319f268a2e1ed2b5dac977f86f7685614c5759a6
Дима <orbitvok.t@gmail.com>
2026-03-23 18:47:39 +0800

    add

---
+++ vue3/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Счётчик: {{ count }}</h1>
+    <button @click="increment">Увеличить</button>
+    <Child message="Привет из родительского компонента!" />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import Child from './components/Child.vue'
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+onMounted(() => {
+  console.log('Компонент App смонтирован!')
+})
+</script>
